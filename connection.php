00aeac05e4bf5e8a9fe1db106d9a9dd5d3da3d18
Simeon Nenov <nenov.simeon@gmail.com>
2014-05-27 21:08:48 +0300

    connection.php

--- connection.php
+++ connection.php
@@ -6,4 +6,4 @@
 	$prefix = "";
 	$bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
 	mysql_select_db($mysql_database, $bd) or die("Could not select database");
-?>fdfd
+?>
